f38bf248e94055169275a1aab3af2bcbd9e6f54f
Igor <i.r.t.monteiro@gmail.com>
2022-06-17 16:23:59 -0300

    criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,13 @@
-class Pessoa:
-    pass
+"""
+Aula sobre Métodos
+"""
+
+class Pessoa:#classe
+    def cumprimentar(self):# método =  função que pertence a uma classe
+        return f"Olá! {id(self)}"
+
+if __name__ == '__main__': ##este é o teste da classe
+    p = Pessoa() #objeto
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
