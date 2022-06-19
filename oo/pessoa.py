83a580a680a1eb8cf6b5503229fcd9f3935492ad
Igor <i.r.t.monteiro@gmail.com>
2022-06-18 21:43:24 -0300

    Criados atributos e instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,6 +3,10 @@ Aula sobre Métodos
 """
 
 class Pessoa:#classe
+    def __init__(self, nome=None, idade=39):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):# método =  função que pertence a uma classe
         return f"Olá! {id(self)}"
 
@@ -11,3 +15,7 @@ if __name__ == '__main__': ##este é o teste da classe
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = "Igor"
+    print(p.nome, p.idade)
+    
