899591817d1e008ecdc265b883ec0fd23faa850b
Igor <i.r.t.monteiro@gmail.com>
2022-06-18 22:36:52 -0300

    criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,19 +3,22 @@ Aula sobre Métodos
 """
 
 class Pessoa:#classe
-    def __init__(self, nome=None, idade=39):
+    def __init__(self, *filhos, nome=None, idade=39):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):# método =  função que pertence a uma classe
         return f"Olá! {id(self)}"
 
 if __name__ == '__main__': ##este é o teste da classe
-    p = Pessoa() #objeto
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = "Igor"
-    print(p.nome, p.idade)
-    
+    renzo = Pessoa(nome='Renzo') #objeto
+    luciano = Pessoa(renzo, nome='Luciano',idade=50) #objeto
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
+   
